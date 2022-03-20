7db9ed8f630cbad8f73fe2b170fa2d3e6d13adcf
SachithG1998 <gunaseka_ps17050@stu.kln.ac.lk>
2022-03-20 08:25:26 +0530

    Revert "Added UI views with a button and text area view"
    This reverts commit 0ef59886b0731a000bfc9a374cdb43937008f4d8.

--- app/src/main/java/com/sachith/android/apipractice/MainActivity.kt
+++ app/src/main/java/com/sachith/android/apipractice/MainActivity.kt
@@ -2,17 +2,10 @@ package com.sachith.android.apipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-
-const val EXTRA_MESSAGE = "com.sachith.android.apipractice.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    
-    /** Called when the user presses the Send button */
-    fun sendMessage(view: View) {
     }
 }
