0ef59886b0731a000bfc9a374cdb43937008f4d8
SachithG1998 <gunaseka_ps17050@stu.kln.ac.lk>
2022-03-20 08:16:31 +0530

    Added UI views with a button and text area view

--- app/src/main/java/com/sachith/android/apipractice/MainActivity.kt
+++ app/src/main/java/com/sachith/android/apipractice/MainActivity.kt
@@ -2,10 +2,17 @@ package com.sachith.android.apipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+const val EXTRA_MESSAGE = "com.sachith.android.apipractice.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    
+    /** Called when the user presses the Send button */
+    fun sendMessage(view: View) {
     }
 }
